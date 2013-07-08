ef1d2c3e3489f95b697a0a8ef5642d8c9ad519d8
yuta.yoshiike <yuta.yoshiike@mixi.co.jp>
2013-07-08 10:42:55 +0900

    Update script
    - YAML を読むように修正

--- shuffle.pl
+++ shuffle.pl
@@ -1,26 +1,40 @@
 use 5.014;
 use common::sense;
+use Getopt::Long qw/GetOptions/;
 use Math::Random::MT::Auto qw/shuffle/;
-my @members = qw/funatsu funagakure tojo watanabe yoshinaga yoshiike/;
-while(my @order_list = shuffle(@members, @members)) {
-    say join ',', @order_list;
-    exit if $order_list[0] eq 'funatsu';
-}
-__END__
-watanabe,funagakure,tojo,funatsu,yoshinaga,yoshiike,watanabe,funagakure,funatsu,yoshinaga,tojo,yoshiike
-yoshiike,funatsu,tojo,funagakure,funatsu,tojo,yoshinaga,yoshinaga,funagakure,watanabe,watanabe,yoshiike
-yoshinaga,funagakure,yoshiike,tojo,funatsu,tojo,yoshiike,watanabe,watanabe,funatsu,yoshinaga,funagakure
-yoshinaga,yoshinaga,yoshiike,funatsu,funatsu,yoshiike,watanabe,watanabe,funagakure,tojo,tojo,funagakure
-yoshinaga,watanabe,yoshiike,funagakure,tojo,yoshinaga,watanabe,yoshiike,funatsu,funatsu,funagakure,tojo
-funagakure,yoshiike,funatsu,watanabe,yoshinaga,funagakure,tojo,tojo,watanabe,funatsu,yoshiike,yoshinaga
-watanabe,yoshinaga,yoshinaga,funatsu,yoshiike,funagakure,watanabe,funatsu,funagakure,tojo,yoshiike,tojo
-tojo,yoshiike,tojo,funatsu,funatsu,yoshinaga,watanabe,funagakure,yoshiike,yoshinaga,funagakure,watanabe
-yoshiike,yoshinaga,funatsu,funagakure,watanabe,funagakure,tojo,funatsu,tojo,yoshiike,yoshinaga,watanabe
-funagakure,tojo,watanabe,yoshinaga,yoshiike,tojo,funatsu,watanabe,funagakure,yoshinaga,yoshiike,funatsu
-yoshiike,tojo,funatsu,funagakure,funagakure,watanabe,watanabe,yoshinaga,yoshinaga,tojo,yoshiike,funatsu
-tojo,watanabe,yoshinaga,funatsu,funagakure,funagakure,funatsu,tojo,yoshiike,yoshiike,yoshinaga,watanabe
-yoshinaga,tojo,funagakure,yoshiike,yoshinaga,funatsu,watanabe,tojo,funatsu,yoshiike,funagakure,watanabe
-tojo,yoshinaga,watanabe,yoshiike,funagakure,yoshiike,funatsu,funagakure,yoshinaga,tojo,watanabe,funatsu
-watanabe,watanabe,funatsu,yoshiike,tojo,funagakure,yoshinaga,yoshinaga,yoshiike,tojo,funatsu,funagakure
-funatsu,yoshinaga,tojo,funatsu,watanabe,yoshiike,funagakure,yoshinaga,tojo,watanabe,funagakure,yoshiike
+use Pod::Usage;
+use YAML::Tiny;
 
+my $yaml_file;
+GetOptions("data=s"=>\$yaml_file);
+pod2usage("ERROR: required '--data' option.") unless $yaml_file;
+my $yaml = eval { YAML::Tiny->read($yaml_file) };
+pod2usage("ERROR: --data '$yaml_file' is yaml file?") unless $yaml;
+
+my $members = $yaml->[0]->{members};
+my $first_member = $yaml->[0]->{first_member};
+while(my @order_list = shuffle(@$members, @$members)) {
+    say join ',', @order_list;
+    exit if $order_list[0] eq $first_member;
+}
+
+=encoding utf8
+
+=head1 NAME
+
+shuffle.pl - yamlファイルに記述されたメンバーリストをシャッフルして返します
+
+=head1 USAGE
+
+    # run script
+
+    $ carton exec -- perl shuffle.pl --data ./data/team_members.yaml
+
+    # yaml file as follows
+
+    members:
+        - foo
+        - bar
+        - baz
+    first_member: baz
+=cut
